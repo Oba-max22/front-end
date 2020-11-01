1b657e02744bb2726d3a7e3add7a69fb68b5cd95
Emmanuel Macaulay <61067933+Oba-max22@users.noreply.github.com>
2020-11-01 12:13:05 +0100

    Update dailyWage.js

--- dailyWage.js
+++ dailyWage.js
@@ -1,17 +1,17 @@
 
 function dailyWage(hours_worked) {
-
-    let daily_pay = 1000 * hours_worked;
-    let extra_pay = 200;
-    let total_pay = 0;
+    var daily_pay = 1000 * hours_worked;
+    var extra_pay = 200;
+    var total_pay = 0;
     if (hours_worked>4) {
         extra_hours = hours_worked - 4;
         const bonus = extra_hours * extra_pay;
-        return total_pay = (daily_pay + (bonus));
+        total_pay = (daily_pay + (bonus));
+        console.log("Your daily wage is " + total_pay);
     } else {
-        return total_pay = (daily_pay);
+        total_pay = (daily_pay);
+        console.log("Your daily wage is " + total_pay);
     }
 }
-let show_wage = dailyWage(8);
-console.log("Daily Wage")
-console.log(show_wage);
+
+dailyWage(8);
